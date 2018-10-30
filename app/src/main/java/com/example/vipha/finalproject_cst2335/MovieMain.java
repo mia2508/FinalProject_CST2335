9f57bea8aba0bad5e2ea18c89d7e1913bf2883ff
CST2335 <pham0101@algonquinlive.com>
2018-10-30 11:43:34 -0400

    add MovieMain page- connect it with movie button

---
+++ app/src/main/java/com/example/vipha/finalproject_cst2335/MovieMain.java
@@ -0,0 +1,13 @@
+package com.example.vipha.finalproject_cst2335;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MovieMain extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_main);
+    }
+}
