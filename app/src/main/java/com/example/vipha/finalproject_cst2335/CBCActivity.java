8ef78b2774291538ae41121a2f9075641e070c79
jonathandan <dan00005@algonquinlive.com>
2018-10-31 18:28:58 -0400

    initia project structure

--- app/src/main/java/com/example/vipha/finalproject_cst2335/CBCActivity.java
+++ app/src/main/java/com/example/vipha/finalproject_cst2335/CBCActivity.java
@@ -3,11 +3,11 @@ package com.example.vipha.finalproject_cst2335;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MovieMain extends Activity {
+public class CBCActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movie_main);
+        setContentView(R.layout.activity_cbc);
     }
 }
---
+++ app/src/main/java/com/example/vipha/finalproject_cst2335/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.vipha.finalproject_cst2335;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    protected static final String ACTIVITY_NAME = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Context ctx = this;
+        // moive
+        Button movieButton = findViewById(R.id.movieButton);
+        movieButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ctx, MovieActivity.class);
+                startActivity(intent);
+            }
+        });
+        //Log.i(ACTIVITY_NAME, "In onCreate()");
+
+
+// bus
+        Button button = findViewById(R.id.bus);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ctx, BusActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // cbc news
+
+        Button cbcnews = findViewById(R.id.CBC);
+        cbcnews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ctx, CBCActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // food nutrition
+        Button food = findViewById(R.id.food);
+        food.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ctx, FoodActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy");
+    }
+
+}
