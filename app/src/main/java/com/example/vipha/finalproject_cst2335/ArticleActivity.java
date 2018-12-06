b0a2b8dbd7bc4c7e8c135e36ee1cc718e35deabc
jonathandan <dan00005@algonquinlive.com>
2018-12-06 10:55:10 -0500

    Final push for demo

--- app/src/main/java/com/example/vipha/finalproject_cst2335/ArticleActivity.java
+++ app/src/main/java/com/example/vipha/finalproject_cst2335/ArticleActivity.java
@@ -1,13 +1,32 @@
 package com.example.vipha.finalproject_cst2335;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.transition.FragmentTransitionSupport;
+import android.support.v7.app.AppCompatActivity;
 
-public class ArticleActivity extends Activity {
+/**
+ * This class is used to transition to the fragment of NewsFragment class after the user clicks
+ * on a new item in the newsListVIew from CBCActivity
+ */
+
+public class ArticleActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_article);
+        setContentView(R.layout.activity_cbc_details);
+
+        Bundle bundle = getIntent().getBundleExtra("News Item");
+        NewsFragment newsFragment = new NewsFragment();
+        newsFragment.setArguments(bundle);
+        android.support.v4.app.FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.articleFrame, newsFragment).commit();
+
+
     }
+
+
 }
